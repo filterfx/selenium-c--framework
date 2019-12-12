732f2c53090ebd104fc34dd736a4050690665883
“filetfx” <maliq.ali@gmail.com>
2019-12-12 16:59:52 +0100

    Script the frst test with an assertion

--- Royale.Tests/CardTests.cs
+++ Royale.Tests/CardTests.cs
@@ -22,7 +22,13 @@ namespace Tests
         [Test]
         public void Ice_Spirit_is_on_the_cards_page()
         {
-            Assert.Pass();
+            // 1. go to statsroyale.com
+            driver.Url = "https://statsroyale.com";
+            // 2. click cards links in header nav
+            driver.FindElement(By.CssSelector("a[href='/cards']")).Click();
+            // 3. Assert ice spirit is displayed
+            var iceSpirit = driver.FindElement(By.CssSelector("a[href='https://statsroyale.com/card/Ice+Spirit']"));
+            Assert.That(iceSpirit.Displayed);
         }
     }
 }
